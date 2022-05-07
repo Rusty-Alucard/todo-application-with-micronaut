7e7edc76a0808e10195e763b23e9daff814283f1
rusty.alucard <rusty0929+develop@gmail.com>
2022-05-07 18:56:41 +0900

    implement persistence layer

---
+++ src/main/kotlin/todo/domain/model/entity/ToDo.kt
@@ -0,0 +1,18 @@
+package todo.domain.model.entity
+
+import io.micronaut.data.annotation.GeneratedValue
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+
+@MappedEntity
+class ToDo(
+    var title: String,
+    val authorId: Long
+) {
+
+    @Id
+    @GeneratedValue(GeneratedValue.Type.AUTO)
+    var id: Long? = null
+
+    var completed = false
+}
